21ad057c95f0396040de52fcc52cf9816fb5258e
yashpola <yashwit.po@gmail.com>
2023-10-27 22:04:45 +0800

    Alice-Bob v2.0: Packet Corruption

--- Assignment2/Alice.py
+++ Assignment2/Alice.py
@@ -4,16 +4,19 @@ from zlib import crc32
 
 
 def main():
-    serverName = ""
-    serverPort = sys.argv[1]
-    clientSocket = socket(AF_INET, SOCK_DGRAM)
+    serverPort = int(sys.argv[1])
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind(("", serverPort))
     while True:
-        message = sys.stdin.read(62)
-        if len(message) == 0:
-            clientSocket.close()
-            break
-        clientSocket.sendto(message.encode(), (serverName, int(serverPort)))
-        modifiedMessage, serverAddress = clientSocket.recvfrom(62)
+        segment, clientAddress = serverSocket.recvfrom(63)
+        expectedChecksum = int.from_bytes(segment[:4], byteorder=sys.byteorder)
+        actualChecksum = crc32(segment[4:])
+        if expectedChecksum != actualChecksum:
+            serverSocket.sendto(b"Packet corrupted", clientAddress)
+            continue
+        sys.stdout.write(segment[4:].decode())
+        sys.stdout.flush()
+        serverSocket.sendto(b"Packet ok", clientAddress)
 
 
 if __name__ == "__main__":
--- Assignment2/Bob.py
+++ Assignment2/Bob.py
@@ -4,15 +4,22 @@ from zlib import crc32
 
 
 def main():
-    serverPort = int(sys.argv[1])
-    serverSocket = socket(AF_INET, SOCK_DGRAM)
-    serverSocket.bind(("", serverPort))
+    serverName = ""
+    serverPort = sys.argv[1]
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+    fullMessage = ""
     while True:
-        message, clientAddress = serverSocket.recvfrom(62)
-        receivedMessage = message.decode()
-        sys.stdout.write(receivedMessage)
-        sys.stdout.flush()
-        serverSocket.sendto(receivedMessage.encode(), clientAddress)
+        message = sys.stdin.read(59)
+        if len(message) == 0:
+            clientSocket.close()
+            break
+        checksum = crc32(message.encode())
+        segment = checksum.to_bytes(4, byteorder=sys.byteorder) + message.encode()
+        clientSocket.sendto(segment, (serverName, int(serverPort)))
+        modifiedMessage, serverAddress = clientSocket.recvfrom(64)
+        while modifiedMessage.decode() != "Packet ok":
+            clientSocket.sendto(segment, (serverName, int(serverPort)))
+            modifiedMessage, serverAddress = clientSocket.recvfrom(64)
 
 
 if __name__ == "__main__":
